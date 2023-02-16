254f85a7b71b329666b13fc6fa1fdacf800b1d84
Gerd Wierichs <wierichs.g@eplan.de>
2023-02-16 06:28:41 +0100

    corrected relative path names

--- FirstTry/sw.js
+++ FirstTry/sw.js
@@ -5,9 +5,9 @@ self.addEventListener('install', event => {
   event.waitUntil((async () => {
     const cache = await caches.open(CACHE_NAME);
     cache.addAll([
-      '/',
-      '/converter.js',
-      '/style.css'
+      './',
+      './converter.js',
+      './style.css'
     ]);
   })());
 });
